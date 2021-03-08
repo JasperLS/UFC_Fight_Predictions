c576bc5e25af921b81d6883221da6ed9ea66d0a8
Mattis <hartwig@ifis.uni-luebeck.com>
2021-03-08 12:58:04 +0100

    Clean up ignored files

--- config.py
+++
@@ -1,9 +0,0 @@
-#Paths
-path_chromedriver = 'C:/Users/Mattis/Documents/Python Scripts/chromedriver.exe'
-path_data_output = 'D:/jasper/98_general_projects/03_UFC_Predictions/Data/2020-11-27_scraped_data/'
-path_project_scraper = 'C:/Users/Mattis/Documents/Python Scripts/UFC/UFC_Fight_Predictions/scraping'
-
-#Sleeping time
-sleep = 0.3
-
-2
